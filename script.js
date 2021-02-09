eada9d58b1afb8b771ab61b34132e445e0a56100
ddmeyer23 <78511312+ddmeyer23@users.noreply.github.com>
2021-02-09 04:50:45 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,44 @@
-require(["esri/Map", "esri/views/MapView"], function(Map, MapView) {
-        var map = new Map({
-          basemap: "dark-gray-vector"
-        });
+var mymap = L.map('mapid').setView([51.490, -0.09], 13);
 
-        var view = new MapView({
-          container: "viewDiv",
-          map: map,
-          zoom: 10,
-          center: [-90.1, 38.46] // longitude, latitude
-        });
-      });
+
+L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 20,
+	ext: 'png'
+}).addTo(mymap);
+
+var marker1 = L.marker([51.5, -0.09]).addTo(mymap);
+
+var marker2 = L.marker([51.525, -0.09]).addTo(mymap);
+
+var circle = L.circle([51.508, -0.11], {
+    color: 'blue',
+    fillColor: '#f55',
+    fillOpacity: 0.85,
+    radius: 1000
+}).addTo(mymap);
+
+var polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(mymap);
+
+marker1.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+marker2.bindPopup("<b>Hello world!</b><br>I am a popup, as well.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
+
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(mymap);
+}
+
+mymap.on('click', onMapClick);
